8c090d8853ec1c441c98679258e986349e24b124
araujoviana <63423848+araujoviana@users.noreply.github.com>
2025-01-28 13:56:14 -0300

    Isolated predefined constants

---
+++ receiver/config.h
@@ -0,0 +1,4 @@
+#define BUZZER_PIN 9 // Must be PWM capable (~) and must match F# code
+#define BAUD_RATE 9600 // Must match F# code
+#define BASE_UNIT 1000 // Base unit for note duration
+#define TONE_PAUSE 50 // Predefined pause between each note
--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,30 +1,27 @@
-#define BUZZER_PIN 9 // HAS to be the same as the one provided in the F# code!
-#define BAUD_RATE 9600 // HAS to be the same as the one provided in the F# code!
-#define BASE_UNIT 1000 // Minimum duration a note plays
-#define TONE_PAUSE 50 // Predefined pause between each note
+#include "config.h"
 
 void setup() {
   Serial.begin(BAUD_RATE);
+
   pinMode(BUZZER_PIN, OUTPUT);
-
   pinMode(LED_BUILTIN, OUTPUT); // For debugging purposes
-
-
 }
 
 void loop() {
-  if (Serial.available() > 0) {
+  if (Serial.available() > 0) { // Check if there is any serial data available
     int note = Serial.parseInt();
     if (note > 0) {
       digitalWrite(LED_BUILTIN, HIGH);
 
-      tone(BUZZER_PIN, note, BASE_UNIT);
+      tone(BUZZER_PIN, note, BASE_UNIT); // Plays a note with the received frequency
       delay(TONE_PAUSE);
-      noTone(BUZZER_PIN);
+
+      // noTone(BUZZER_PIN);
 
       digitalWrite(LED_BUILTIN, LOW);
     }
-    while(Serial.available() > 0) {
+    // Clear remaining data in the serial buffer
+    while(Serial.available() > 0) { 
       Serial.read();
     }
   }
